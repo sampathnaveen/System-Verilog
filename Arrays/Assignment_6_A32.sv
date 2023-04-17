6e5eedc76f01d65e5480bdab0ea34efa086dd603
Sampath Naveen <64924592+sampathnaveen@users.noreply.github.com>
2023-04-17 11:42:41 -0400

    Add files via upload

---
+++ Arrays/Assignment_6_A32.sv
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/12/2023 12:42:58 AM
+// Design Name: 
+// Module Name: Assignment_6_A32
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Assignment_6_A32();
+
+endmodule
